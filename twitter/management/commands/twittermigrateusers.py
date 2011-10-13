c23fbd4be6172f82cc9d4e4872e7f463157b24dd
Scott Turnbull <sturnbu@emory.edu>
2011-10-13 17:24:46 -0400

    Added seperate user, geo and coord models and capturing local info.

---
+++ twitter/management/commands/twittermigrateusers.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+
+from twap.twitter.streamreader import StreamHarvest
+from twap.twitter.models import TwitterUser, Tweet
+
+class Command(BaseCommand):
+    help = ''
+    args = ''
+
+    def handle(self, **options):
+        tweets = Tweet.objects.all()
+        for tweet in tweets:
+            tuser, created = TwitterUser.objects.get_or_create(screen_name=tweet.screen_name)
+            tuser.twitter_id = tweet.user_id
+            tuser.save()
+            tweet.twitter_user = tuser
+            tweet.save()
