c6a3e079e1ae8e2c7a6223342d2d82778a73b43f
Scott Turnbull <sturnbu@emory.edu>
2011-10-14 15:03:25 -0400

    Saving stray changes before a new branch.

--- twitter/management/commands/twitteruserstats.py
+++ twitter/management/commands/twitteruserstats.py
@@ -8,4 +8,18 @@ class Command(BaseCommand):
     args = ''
 
     def handle(self, **options):
-        pass
+        """
+        This is helps write some basic aggregation data about users and their tweets.
+
+        It should accomplish 2 things.
+
+            *  Save a list of individual users screen_name, location, # of tweets
+            * Save a list of # of tweets, # of users with those number of tweets.
+
+            Sandemous Highschool Football Rules!
+        """
+        users = TwitterUser.objects.all()
+        for user in users:
+            tweet_count = user.tweet_set.all().count()
+            
+            
