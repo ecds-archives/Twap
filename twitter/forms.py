ae933c94c11dfe20aee22ab1a3be4506005d1a2a
Scott Turnbull <sturnbu@emory.edu>
2011-11-15 15:44:48 -0500

    More theme improvements.

--- twitter/forms.py
+++ twitter/forms.py
@@ -5,13 +5,16 @@ class TagForm(forms.Form):
     A form for searching and filtering hashtags.
     """
     search_help = 'Comma seperated list of partial search words.'
-    search = forms.CharField(max_length=50, help_text=search_help, required=False)
+    s_widget = forms.TextInput(attrs={'class': 'text'})
+    search = forms.CharField(max_length=50, help_text=search_help, required=False, widget=s_widget)
     ex_help = 'Comma seperated list of partial words to exclude.'
-    exclude = forms.CharField(max_length=50, help_text=ex_help,required=False)
+    ex_widget = forms.TextInput(attrs={'class': 'text'})
+    exclude = forms.CharField(max_length=50, help_text=ex_help,required=False,widget=ex_widget)
 
 class UserSearchForm(forms.Form):
     """
     Simple search for twitter usernames.
     """
     search_help = 'Username (full or partial) to search for.'
-    search = forms.CharField(max_length=50, help_text=search_help, label='Twitter Username')
+    s_widget = forms.TextInput(attrs={'class': 'text'})
+    search = forms.CharField(max_length=50, help_text=search_help, label='Twitter Username', widget=s_widget)
