d333216dbaafd45b14ef0aca682c9b6a64cea1bf
Scott Turnbull <streamweaver@gmail.com>
2012-06-15 14:21:12 -0400

    Temp change to try a smaller set

--- twitter/management/commands/twap_dump_json.py
+++ twitter/management/commands/twap_dump_json.py
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 
     def _write_month(self, month, year):
         # tweets = Tweet.objects.filter(created_at__month=month, created_at__year=year).prefetch_related()
-        tweets = Tweets.objects.all()[:10000]
+        tweets = Tweet.objects.all()[:10000]
         outfile = open('../twap_dump_%s_%s.json' % (month, year), 'wb')
         for tweet in tweets:
             data = {
